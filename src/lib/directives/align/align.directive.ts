d206bde23db90a927fba909f25995b479efebbd6
type0code <type0code@gmail.com>
2023-06-27 13:46:31 +0300

    updated to angular 16

--- src/lib/directives/align/align.directive.ts
+++ src/lib/directives/align/align.directive.ts
@@ -6,7 +6,7 @@ import { align } from '../../interfaces/general';
 })
 export class AlignDirective {
   @Input() tcAlign: align;
-  @HostBinding('class.start-align') get leftAlign() { return this.tcAlign === align.start };
+  @HostBinding('class.start-align') get startAlign() { return this.tcAlign === align.start };
   @HostBinding('class.center-align') get centerAlign() { return this.tcAlign === align.center };
-  @HostBinding('class.end-align') get rightAlign() { return this.tcAlign === align.end };
+  @HostBinding('class.end-align') get endAlign() { return this.tcAlign === align.end };
 }
